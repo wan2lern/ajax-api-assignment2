f247cd868c3aa047553a788bd0be76590106fd2d
Daniel Karjanlahti <d@Daniels-MacBook-Pro.local>
2016-08-28 15:59:23 +0200

    Changed the names of weather icons to match with API response

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 // document.ready
 $(function () {
-    $('#get-sthlm').get('http://api.openweathermap.org/data/2.5/weather?q=Stockholm&APPID=febb6f8b574869296fae74debbb6d211&units=metric', function (data) {
+    var appID = 'febb6f8b574869296fae74debbb6d211&units';
+    var OpenWeatherURL = 'http://api.openweathermap.org/data/2.5/weather?';
+    var prepare = function () {
+        var city = $('#city').val();
+        if (city && city != '') {
+            city = 'q=' + city.trim();
+            getData(city, OpenWeatherURL, appID);
+        }
+    }
 
+    $('#get-weather').on('click', function (e) {
+        e.preventDefault();
+        $.get('http://api.openweathermap.org/data/2.5/weather?q=Stockholm&APPID=' + appid + '=metric', function (data, status) {
+            if (status == 'success') {
+
+                console.log(data);
+            } else {
+
+            }
+        });
     });
-    $('#get-uk').get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=febb6f8b574869296fae74debbb6d211&units=metric', function (data) {
-
-    });
-
 });
